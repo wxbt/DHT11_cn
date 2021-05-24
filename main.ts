5023e40887b35d765bffd239be8ca826974fd66a
wxbt <50980203+wxbt@users.noreply.github.com>
2021-05-24 15:57:31 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% color=#1C86EE icon="\uf043" block="外设_温湿度传感器"
+//% color=#1C86EE icon="\uf043" block="B_温湿度传感器"
 namespace dht11 {
     let pin = DigitalPin.P0;
     function signal_dht11(pin: DigitalPin): void {
